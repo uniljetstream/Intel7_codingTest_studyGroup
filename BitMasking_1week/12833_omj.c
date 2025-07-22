20d9380fe2793a1d8f38c12b80d39f07fbe9e2fe
ohminji <ohminji426@naver.com>
2025-07-22 17:49:34 +0900

    XORXORXOR비트마스킹

---
+++ BitMasking_1week/12833_omj.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B, C;
+	int result;
+	
+	scanf("%d %d %d", &A, &B, &C);
+	if (C % 2)
+	{
+		result = A ^ B;
+	}
+	else
+	{
+		result = (A ^ B) ^ B;
+	}
+	printf("%d", result);
+
+	return 0;
+}
