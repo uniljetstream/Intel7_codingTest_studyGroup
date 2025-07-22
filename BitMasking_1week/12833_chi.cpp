7ccfe08b71698e5d61650a9edf7a28e73f00fb70
chi <cldud97@naver.com>
2025-07-22 10:24:43 +0900

    코드 정렬 적용

--- BitMasking_1week/12833_chi.cpp
+++ BitMasking_1week/12833_chi.cpp
@@ -7,22 +7,22 @@ int main()
 {
     freopen("input.txt", "r", stdin);
     long long a, b, c;
-    
+
     cin >> a >> b >> c;
 
-    if(c%2==0)
+    if (c % 2 == 0)
     {
         cout << a;
     }
     else
-        cout << (a^b);
+        cout << (a ^ b);
     // 13 xor 3 > 14
     // 14 xor 3 > 13
 
-    //13 > 1101
-    // 3 > 0011
-    // 1110
-    // 0011
+    // 13 > 1101
+    //  3 > 0011
+    //  1110
+    //  0011
 
     // 1101
 
