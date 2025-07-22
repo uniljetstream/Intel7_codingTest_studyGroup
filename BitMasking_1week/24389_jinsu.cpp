f976f8d2f8e55c611509bbd2d61051665bde3706
박진수 <withanjel123@naver.com>
2025-07-22 12:03:23 +0900

    v2.1

--- BitMasking_1week/24389_jinsu.cpp
+++ BitMasking_1week/24389_jinsu.cpp
@@ -11,11 +11,17 @@ int main()
 	a_two_comple = ~a + 1;
 
 	int cnt = 0;
+	int mask = 1;
 	for (int i = 0; i < 32; i++)
-	{
-		if ((a << i )^ (a_two_comple << i))
+	{	
+		
+		
+		if ((a &mask) ^ (a_two_comple & mask))
 			cnt++;
+		
+		mask *= 2;
+		
 	}
-
+	
 	printf("%d", cnt);
 }
