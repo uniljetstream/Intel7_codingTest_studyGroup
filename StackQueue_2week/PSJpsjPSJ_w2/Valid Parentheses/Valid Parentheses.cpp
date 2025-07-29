d58fd2d195ec017205034adb5fc9878d09563940
PSJpsjPSJ <psjy33@skuniv.ac.kr>
2025-07-29 20:38:21 +0900

    stack save

---
+++ StackQueue_2week/PSJpsjPSJ_w2/Valid Parentheses/Valid Parentheses.cpp
@@ -0,0 +1,35 @@
+﻿#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    stack<char> st;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(' || st.empty())
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')' and st.top() == '(')
+        {
+            st.pop();
+        }
+    }
+    if (st.empty() == true)
+    {
+        answer = true;
+    }
+    else
+    {
+        answer = false;
+    }
+//    }
+
+    return answer;
+}
