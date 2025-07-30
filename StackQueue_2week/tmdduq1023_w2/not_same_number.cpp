a8810159566227124bbdbd5f7df4b03576ed989f
SHIN SEUNG YUP <1023tmdduq@naver.com>
2025-07-30 16:39:21 +0900

    프로그래머스 같은 숫자는 싫어 문제,풀이 완료

---
+++ StackQueue_2week/tmdduq1023_w2/not_same_number.cpp
@@ -0,0 +1,31 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    vector<int> nanswer;
+    int num = -1;
+    while (!(arr.empty()))
+    {
+        if (num != arr.back())
+        {
+            nanswer.push_back(arr.back());
+        }
+        num = arr.back();
+        arr.pop_back();
+    }
+    cout << endl;
+    //answer.push_back(num);
+    int size = nanswer.size();
+    cout << size << endl;
+    for (int i = 0; i < size; i++)
+    {
+        answer.push_back(nanswer.back());
+        nanswer.pop_back();
+    }
+
+    return answer;
+}
