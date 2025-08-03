4969deba97dc1d983cdad3ea4305b63fec8e3297
김민우 <2018440019@office.uos.ac.kr>
2025-08-03 16:15:16 +0900

    스택/큐-탑(시간초과)

--- StackQueue_2week/StackQueue/탑/main_kmw.cpp
+++ StackQueue_2week/StackQueue/탑/main_kmw.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
-#include <stack>
+#include <vector>
+#include <utility>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N, x, index, maxTowerLevel = 0;
+	int N, x, index = 0;
 	cin >> N;
 
-	stack<int> tower;
+	vector<pair<int, int>> tower;
+	vector<pair<int, int>>::reverse_iterator it; // 역 반복자
+	vector<int> indexOfReceivedTower;
+	pair<int, int> p;
 
-	for (int i = 0; i < N; i++) {
+	cin >> x;
+	tower.push_back({ x, 0 });
+	indexOfReceivedTower.push_back(index);
+
+	for (int i = 1; i < N; i++) {
 		cin >> x;
-		if (x >= maxTowerLevel) {
-			maxTowerLevel = x;
-			index = i;
+		index = 0;
+		for (it = tower.rbegin(); it != tower.rend(); ++it) {
+			if (it->first > x) {
+				index = it->second + 1;
+				break;
+			}
 		}
-		
-		
-		tower.push(x);
-		
+		indexOfReceivedTower.push_back(index);
+
+		tower.push_back({ x, i });
+	}
+	for (int i = 0; i < N; i++) {
+		cout << indexOfReceivedTower[i] << ' ';
 	}
 }
