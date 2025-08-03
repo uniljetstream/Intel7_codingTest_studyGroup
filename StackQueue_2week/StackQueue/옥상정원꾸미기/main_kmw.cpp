dfd5eb2cf74819aa82503727becf3613f02be71a
김민우 <2018440019@office.uos.ac.kr>
2025-08-03 17:27:57 +0900

    스택/큐-옥상 정원 가꾸기(시간초과 해결)

--- StackQueue_2week/StackQueue/옥상정원꾸미기/main_kmw.cpp
+++ StackQueue_2week/StackQueue/옥상정원꾸미기/main_kmw.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N, h, cnt = 0;
+	int N, h;
+	unsigned int cnt = 0;
 	cin >> N;
 	
-	vector<int> s;
-	vector<int>::reverse_iterator it;
-
+	stack<int> s;
+	
 	for (int i = 0; i < N; i++) {
 		cin >> h;
-		s.push_back(h);
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = i + 1; j < N; j++) {
-			if (s[i] > s[j]) {
-				cnt++;
-			}
-			else {
-				break;
-			}
+		while (!s.empty() && s.top() <= h) {
+			s.pop();
 		}
+		cnt += s.size();
+		s.push(h);
 	}
 
 	cout << cnt;
