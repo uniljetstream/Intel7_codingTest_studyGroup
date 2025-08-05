2623fc4a66700388e272cb7d25fce67f331e8e45
YunChiyeong <cldud97@naver.com>
2025-08-05 19:17:53 +0900

    좀더 맵게(작성중)

---
+++ Heap_3week/yunchiyeong/scoville.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(const int& a, const int& b)
+{
+    return a < b;
+}
+
+int solution(vector<int> scoville, int K)
+{
+    int answer = 0;
+
+    return answer;
+}
+
+int main()
+{
+    solution({1, 2, 3, 9, 10, 12}, 7);
+    return 0;
+}
