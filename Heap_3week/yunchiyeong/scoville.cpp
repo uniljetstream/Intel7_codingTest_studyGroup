f1b4294e50667f835fb12f86829da38bcf2f18bd
SugarG-G <cldud97@naver.com>
2025-08-05 21:07:16 +0900

    더 맵게

--- Heap_3week/yunchiyeong/scoville.cpp
+++ Heap_3week/yunchiyeong/scoville.cpp
@@ -1,23 +1,37 @@
+#include <string>
+#include <vector>
 #include <iostream>
-#include <queue>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
-bool cmp(const int& a, const int& b)
-{
-    return a < b;
-}
-
-int solution(vector<int> scoville, int K)
-{
+int solution(vector<int> scoville, int K) {
     int answer = 0;
+    int calcScoville = 0;
+    int minScoville = 0;
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
 
+    while(K > pq.top())
+    {
+        if(answer >= scoville.size() - 1) return -1;
+        minScoville = pq.top();
+        pq.pop();
+        calcScoville = minScoville + (pq.top() * 2);
+        pq.pop();
+        pq.push(calcScoville);
+        answer++;
+    }
+    
     return answer;
 }
-
+//
 int main()
 {
-    solution({1, 2, 3, 9, 10, 12}, 7);
+    cout << solution({1, 2, 3, 9, 10, 12}, 7) << endl;
+    cout << solution({7, 8, 11, 9, 10, 12}, 7) << endl;
+    cout << solution({6, 6, 7, 6, 6, 6}, 7) << endl;
+    cout << solution({1, 2, 3, 9, 10, 12}, 50);
+
     return 0;
 }
