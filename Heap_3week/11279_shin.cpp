f2cf5fe7444371f654d919dc193993a1c34ef539
SHIN SEUNG YUP <1023tmdduq@naver.com>
2025-08-07 15:04:48 +0900

    백준 최대 힙 문제,풀이 완료

---
+++ Heap_3week/11279_shin.cpp
@@ -0,0 +1,36 @@
+﻿#include <queue>
+#include <iostream>
+using namespace std;
+int main()
+{
+	priority_queue<int> maxHeap;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+
+		if (num > 0)
+		{
+			maxHeap.push(num);
+		}
+		else
+		{
+			if (maxHeap.empty())
+			{
+				cout << 0 << "\n";
+			}
+			else
+			{
+				cout << maxHeap.top() << "\n";
+				maxHeap.pop();
+			}
+		}
+	}
+
+	return 0;
+}
