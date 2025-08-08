596aea24a541a1a7902127d84258bf0d0075e23b
uniljetstream <test@test.com>
2025-08-08 14:03:53 +0900

    PSJpsJPSJ_w3

---
+++ Heap_3week/PSJpsjPSJ_w3/number_K/number_K.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for (auto& cmd : commands)
+    {
+        int i = cmd[0];
+        int j = cmd[1];
+        int k = cmd[2];
+        vector<int> sub(array.begin() + (i - 1), array.begin() + j);
+        sort(sub.begin(), sub.end());
+        answer.push_back(sub[k - 1]);
+    }
+    return answer;
+}
