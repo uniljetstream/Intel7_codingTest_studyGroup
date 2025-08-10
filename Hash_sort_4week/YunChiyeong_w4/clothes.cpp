7a2382c64f74229e1d0bb8a047877bd335cd23bc
SugarG-G <cldud97@naver.com>
2025-08-10 22:15:38 +0900

    작성중

--- Hash_sort_4week/YunChiyeong_w4/clothes.cpp
+++ Hash_sort_4week/YunChiyeong_w4/clothes.cpp
@@ -8,7 +8,13 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
     int answer = 0;
 
-    unordered_map<string, string> u_map;
+    unordered_multimap<string, string> u_m_map;
+
+    for(auto a : clothes)
+    {
+        cout << a.at(0) << " " << a.at(1) << endl;
+        u_m_map.emplace(a.at(0), a.at(1));
+    }
     
 
     return answer;
@@ -18,5 +24,5 @@ int solution(vector<vector<string>> clothes) {
 int main()
 {
     solution({{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}});
-    solution({{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}});
+    // solution({{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}});
 }
