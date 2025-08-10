35a22dbb62a8776d1114587c89906a78528bbdd4
SugarG-G <cldud97@naver.com>
2025-08-10 21:52:58 +0900

    의상(작성중)

---
+++ Hash_sort_4week/YunChiyeong_w4/clothes.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    return answer;
+}
+
+
+int main()
+{
+    solution({{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}});
+    solution({{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}});
+}
