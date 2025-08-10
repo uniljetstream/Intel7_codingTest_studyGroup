d894f3c7ed9f390edea9cdf1e808e731e5146187
SugarG-G <cldud97@naver.com>
2025-08-10 21:45:35 +0900

    비밀번호 찾기

--- Hash_sort_4week/YunChiyeong_w4/17219.cpp
+++ Hash_sort_4week/YunChiyeong_w4/17219.cpp
@@ -15,7 +15,7 @@ int main()
 
     int n = 0, m = 0;
     map<string, string> ma;
-    string site, passwd;
+    string site, passwd, search;
     cin >> n >> m;
     
     for(int i = 0; i < n; i++)
@@ -23,6 +23,13 @@ int main()
         cin >> site >> passwd;
         ma.emplace(site, passwd);
     }
-    
 
-}
+    for(int i = 0; i < m; i++)
+    {
+        cin >> search;
+        if(auto searchmap = ma.find(search); searchmap != ma.end())
+        {
+            cout << searchmap->second << '\n';
+        }
+    }
+}   
