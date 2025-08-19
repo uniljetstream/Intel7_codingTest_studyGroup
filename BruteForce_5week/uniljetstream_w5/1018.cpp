17e26060d6e811ed08d8a7c0c4f64c1cf940c4fa
uniljetstream <email@email.com>
2025-08-19 21:22:56 +0900

    cowNum

--- BruteForce_5week/uniljetstream_w5/1018.cpp
+++ BruteForce_5week/uniljetstream_w5/1018.cpp
@@ -1,31 +1,36 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
     int n, m;
-    string line;
-    vector<vector<char>> woodBoard;
-    vector<int> checkedBoard;
+    int Wcnt = 0, Bcnt = 0;
 
     cin >> n >> m;
 
-    for(int i=0;i<n;i++)
+    // 그냥 패턴을 비교할래 == 슬라이딩 윈도우
+    string patterW = "WBWBWBWB";
+    string patterB = "BWBWBWBW";
+
+    string line;
+    vector<string> woodBoard;
+
+    for (int i = 0; i < n; i++)
     {
         getline(cin, line);
-        for(char l:line)
-            (woodBoard.at(i)).push_back(l);
+        woodBoard.push_back(line);
     }
-        
-    // 1. 시작이 W인 경우 틀린 경우로 checkedBoard에 표시
 
-    // 1.2. 8x8 칸의 갯수를 세서 최소로 하는 8x8 갯수찾기
-
-    // 2. 시작이 B인 경우 틀린 경우로 checkedBoard에 표시
-
-    // 2.1. 8x8 칸의 갯수를 세서 최소로 하는 8x8 갯수찾기
+    // patterW
+    while(1)
+    {
+        for (int i = 0; i < 8 + i && (8 + i) < m; i++)
+        {
+                 
+        }
+    }
 }
