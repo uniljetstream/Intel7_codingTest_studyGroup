427d5d65810d3909b80feaa33ec211222b7dc556
PSJpsjPSJ <psjy33@skuniv.ac.kr>
2025-08-20 15:03:33 +0900

    동전O만 풀었음

---
+++ BruteForce_5week/PSJpsjPSJ_w5/11047/11047.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int N, K, coin;
+    coin = 0;
+    vector<int> coins(N);
+    cin >> N >> K;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> coins[i];
+    }
+    for (int i = N - 1; i >= 0; i--)
+    {
+        if (K >= coins[i])
+        {
+            coin += K / coins[i];
+            K = K % coins[i];
+        }
+    }
+    cout << coin << '\n';
+    return 0;
+}
