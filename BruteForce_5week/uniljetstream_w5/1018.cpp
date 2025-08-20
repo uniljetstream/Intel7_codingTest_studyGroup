9f6164ef9db0199615613a6c4b392ba698bef91a
uniljetstream <email@email.com>
2025-08-20 14:20:38 +0900

    1018 fin

--- BruteForce_5week/uniljetstream_w5/1018.cpp
+++ BruteForce_5week/uniljetstream_w5/1018.cpp
@@ -9,28 +9,66 @@ int main()
 {
     int n, m;
     int Wcnt = 0, Bcnt = 0;
+    int minVal = 1000000000;
+    string line;
 
     cin >> n >> m;
 
+    vector<vector<char>> woodBoard(n, vector<char>(m, 0));
+
     // 그냥 패턴을 비교할래 == 슬라이딩 윈도우
-    string patterW = "WBWBWBWB";
-    string patterB = "BWBWBWBW";
+    vector<vector<char>> patterW = {{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}, {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                                    {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}, {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                                    {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}, {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                                    {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}, {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}};
 
-    string line;
-    vector<string> woodBoard;
+    vector<vector<char>> patterB = {
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}, {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}, {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}, {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}, {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+    };
 
+    cin.ignore();
+    // 1. 입력받아서 벡터로 저장.
     for (int i = 0; i < n; i++)
     {
-        getline(cin, line);
-        woodBoard.push_back(line);
+        cin >> line;
+        for (int j = 0; j < line.size(); j++)
+            woodBoard.at(i).at(j) = line.at(j);
     }
 
-    // patterW
-    while(1)
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << woodBoard.at(i).at(j) << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // 2. 슬라이딩 원도우 이동하면서 불일치 갯수를 저장. 그 중 최소를 리턴
+
+    // 1. 맨 위가 시작 좌표에서 오른쪽으로 한칸씩 이동하면서 비교
+    bool flag = false;
+    for (int i = 0; i <= n - 8; i++)
     {
-        for (int i = 0; i < 8 + i && (8 + i) < m; i++)
+        for (int j = 0; j <= m - 8; j++)
         {
-                 
+            Wcnt = 0;
+            Bcnt = 0;
+            for (int k = 0; k < 8; k++)
+            {
+                for (int l = 0; l < 8; l++)
+                {
+                    if (woodBoard.at(i + k).at(j + l) != patterW.at(k).at(l))
+                        Wcnt++;
+                    if (woodBoard.at(i + k).at(j + l) != patterB.at(k).at(l))
+                        Bcnt++;
+                }
+            }
+            minVal = min(min(Wcnt, Bcnt), minVal);
         }
     }
+
+    cout << minVal;
 }
