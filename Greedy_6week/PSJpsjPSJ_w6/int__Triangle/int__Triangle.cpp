aed174a39355b991f926a3a11f80229b36fa2e22
PSJpsjPSJ <psjy33@skuniv.ac.kr>
2025-08-29 16:13:44 +0900

    초급 문제만 품

---
+++ Greedy_6week/PSJpsjPSJ_w6/int__Triangle/int__Triangle.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    int n = triangle.size();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    dp[0][0] = triangle[0][0];
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            if (j == 0)
+            {
+                dp[i][j] = dp[i - 1][j] + triangle[i][j];
+            }
+            else if (j == i)
+            {
+                dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+            }
+        }
+    }
+    return *max_element(dp[n-1].begin(), dp[n-1].end());
+}
